041b342f8f77b10dddddaea026c6dab3caf7f3cb
fatimaamin0313-max <fatimaamin0313@gmail.com>
2026-07-13 12:22:03 +0500

    Add files via upload

---
+++ task#4.py
@@ -0,0 +1,20 @@
+num1 = float(input("enter number:"))
+num2 = float(input("enter number:"))
+print("add:", num1 + num2)
+
+
+num1 = float(input("enter number:"))
+num2 = float(input("enter number:"))
+print("subtract:", num1 - num2)
+
+
+num1 = float(input("enter number:"))
+num2 = float(input("enter number:"))
+print(num1 * num2)
+
+
+num1 = float(input("enter number:"))
+num2 = float(input("enter number:"))
+print(num1 / num2)
+
+
