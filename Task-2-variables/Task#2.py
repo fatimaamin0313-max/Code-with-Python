0c416e33ebce06348faed5df9c8f93f50335e1dc
fatima amin <fatimaamin0313@gmail.com>
2026-07-24 13:44:04 +0500

    Task-15

---
+++ Task-2-variables/Task#2.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+print("welcome,"+ name )
+
+father_name= input("Ente your father_name: ")
+print("Your father name is", father_name)
+
+age = input("Enter your age: ")
+print("Your Matric marks is",marks, "Marks")
+
+marks = input("Enter your Matric marks: ")
+print("Your Intermediate Marks is",marks, "Marks")
+
+marks = input("Enter your Intermediate Marks: ")
+print("Your Intermediate Marks is",marks, "Marks.")
+
+program = input("Enter your Desired Program: ")
+print = input("Your Desired Program is", program, "program")
+
