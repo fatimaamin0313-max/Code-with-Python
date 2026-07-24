8901dfc9785c08f7b12643d601ca971f27848224
fatima amin <fatimaamin0313@gmail.com>
2026-07-24 12:00:05 +0500

    created folder for Task-1

---
+++ Task-1-calculator/Calculater (1).py
@@ -0,0 +1,16 @@
+
+num1 = float(input("Enter  number: "))
+num2 = float(input("Enter  number: "))
+print(num1+num2)
+
+num1 = float(input("Enter number: "))
+num2 = float (input("Enter number: "))
+print(num1-num2)
+
+num1 = float(input("Enter number: "))
+num2 = float(input("Enter number: "))
+print(num1*num2)
+
+num1 = float(input("Enter number: "))
+num2 = float(input("Enter number: "))
+print(num1/num2)
